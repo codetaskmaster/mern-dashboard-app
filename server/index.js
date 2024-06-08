692046d440daff8c336c7c3399f42cad72aaa171
Kipruto Barno <barxwells@gmail.com>
2024-06-08 15:20:05 +0300

    build(backend): setup directories

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ import helmet from 'helmet';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 
+import clientRoute from './routes/client.js';
+import generalRoute from './routes/general.js';
+import managementRoute from './routes/management.js';
+import salesRoute from './routes/sales.js';
+
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -15,6 +20,12 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+/* Routes */
+app.use('/client', clientRoute);
+app.use('/general', generalRoute);
+app.use('/management', managementRoute);
+app.use('/sales', salesRoute);
 
 const PORT = process.env.PORT;
 
