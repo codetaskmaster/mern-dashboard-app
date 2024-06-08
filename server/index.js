97c91564e6425e51e91b40ad01f4da2b98b2476e
Kipruto Barno <barxwells@gmail.com>
2024-06-08 13:27:28 +0300

    build(backend): configure and setup

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
